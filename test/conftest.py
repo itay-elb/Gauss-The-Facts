c73b11869462b3ba493885ce18b13c24fd97dbb3
itay <itayelbachr@gmail.com>
2024-01-21 14:56:02 +0200

    big cahnge

--- test/conftest.py
+++ test/conftest.py
@@ -1,11 +1,35 @@
 import pytest
+from pytest_docker import docker_services
+import mysql.connector
 
+@pytest.fixture(scope="module")
+def mysql_service(docker_services):
+    service = docker_services.start('mysql',
+        environment={
+            'MYSQL_ROOT_PASSWORD': 'root',
+            'MYSQL_DATABASE': 'project'
+        },
+        network='protest_default')
+    docker_services.wait_until_responsive(timeout=30, host='mysql', port=3307)
+    yield service
+    service.remove()
 
-@pytest.fixture()
+@pytest.fixture
+def db(mysql_service):
+    mydb = mysql.connector.connect(
+        host='mysql',
+        user='root',
+        passwd='root',
+        database='project'
+    )
+    yield mydb
+    mydb.close()
+
+@pytest.fixture
 def app():
-    from src.app import app
-    yield app
+   from src.app import app
+   yield app
 
 @pytest.fixture()
 def client(app):
-    return app.test_client()
+   return app.test_client()
--- test/test_project.py
+++ test/test_project.py
@@ -1,16 +1,17 @@
+from src.app import mydb
+mycursor = mydb.cursor()
+
 def test_home(client):
-    response=client.get("/")
+    response = client.get("/")
     assert b"<title>Guss The Facts</title>" in response.data
 
 def test_add(client):
-    response=client.get("/add")
+    response = client.get("/add")
     assert b"<title>Add Facts</title>" in response.data
 
 def test_submit_only_one_fact_add(client,app):
     response = client.post("/submit_fact", data={"fact":"check"})
     with (app.app_context()):
-        from src.app import mydb
-        mycursor = mydb.cursor()
         sql="SELECT * FROM project.fansfacts where fact = 'check'"
         mycursor.execute(sql)
         if (mycursor.fetchall() == []):
@@ -19,10 +20,8 @@ def test_submit_only_one_fact_add(client,app):
             assert False
 
 def test_submit_only_two_fact_add(client,app):
-    response = client.post("/submit_fact", data={"fact":"check","qutsion":"if?"})
+    response = client.post("/submit_fact", data={"fact":"check","question":"if?"})
     with (app.app_context()):
-        from src.app import mydb
-        mycursor = mydb.cursor()
         sql="SELECT * FROM project.fansfacts where fact = 'check'"
         mycursor.execute(sql)
         if (mycursor.fetchall() == []):
@@ -31,10 +30,8 @@ def test_submit_only_two_fact_add(client,app):
             assert False
 
 def test_submit_only_three_fact_add(client, app):
-    response = client.post("/submit_fact",data={"fact": "check", "qutsion": "if?", "true_option": "yes"})
+    response = client.post("/submit_fact",data={"fact": "check", "question": "if?", "true_option": "yes"})
     with (app.app_context()):
-        from src.app import mydb
-        mycursor = mydb.cursor()
         sql = "SELECT * FROM project.fansfacts where fact = 'check'"
         mycursor.execute(sql)
         if (mycursor.fetchall() == []):
@@ -43,10 +40,8 @@ def test_submit_only_three_fact_add(client, app):
             assert False
 
 def test_submit_only_four_fact_add(client,app):
-    response = client.post("/submit_fact", data={"fact":"good","qutsion":"try?","true_option":"yes","false_option":"no"})
+    response = client.post("/submit_fact", data={"fact": "good", "question": "try?", "true_option": "yes", "false_option": "no"})
     with (app.app_context()):
-        from src.app import mydb
-        mycursor = mydb.cursor()
         sql="SELECT * FROM project.fansfacts where fact = 'good'"
         mycursor.execute(sql)
         if(mycursor.fetchall() != []):
@@ -57,15 +52,6 @@ def test_submit_only_four_fact_add(client,app):
 def test_restart(client,app):
     response=client.post("/delete")
     with (app.app_context()):
-        from src.app import mydb
-        mycursor = mydb.cursor()
         sql="delete from project.fansfacts where fact = 'good'"
         mycursor.execute(sql)
         assert True
-
-
-
-
-
-
-
