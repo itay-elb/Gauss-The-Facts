99c2bb9a24942a79a5bc7dde10082ff3d36a2249
Lior Amar <lioramar8991@gmail.com>
2024-01-29 15:52:22 +0200

    testing2

--- test/conftest.py
+++ test/conftest.py
@@ -3,20 +3,29 @@ import mysql.connector
 
 @pytest.fixture
 def db():
+    # Get the MySQL service IP address from services.db.ports
+    mysql_service_ip = "127.0.0.1"
+    for port_info in pytest.config.getoption("--docker-services-ports"):
+        if "db" in port_info:
+            mysql_service_ip = port_info.split(":")[0]
+            break
+
     mydb = mysql.connector.connect(
-        host='mysql',
+        host=mysql_service_ip,
         user='root',
         passwd='root',
-        database='project'
+        database='project',
+        port=3306
     )
     yield mydb
     mydb.close()
 
 @pytest.fixture
 def app():
-   from src.app import app
-   yield app
+    from src.app import app
+    yield app
 
 @pytest.fixture()
 def client(app):
-   return app.test_client()
+    return app.test_client()
+
