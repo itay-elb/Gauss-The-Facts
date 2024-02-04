23b49140fba1fa56031e8fcf256ccd259902fe79
itay <itayelbachr@gmail.com>
2024-02-04 19:49:34 +0200

    change env

--- test/conftest.py
+++ test/conftest.py
@@ -1,14 +1,15 @@
 import pytest
 import mysql.connector
+import os
 
 @pytest.fixture
 def db():
     mydb = mysql.connector.connect(
-        host='mysql',
-        user='root',
-        passwd='root',
-        database='project',
-        port=3306
+        host=os.getenv('DB_HOST'),
+        user=os.getenv('DB_USER'),
+        passwd=os.getenv('DB_PASSWD'),
+        database=os.getenv('DB_NAME'),
+        port=os.getenv('DB_PORT')
     )
     yield mydb
     mydb.close()
